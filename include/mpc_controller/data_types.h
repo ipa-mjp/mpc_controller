319faf247b7cf5a715ab6f39aa984b7f6764a056
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-11-01 14:17:03 +0100

    common data for classes

--- include/mpc_controller/data_types.h
+++ include/mpc_controller/data_types.h
@@ -18,6 +18,49 @@ struct JointStates
     KDL::JntArray last_q_dot_;
 };
 
+//--------------------------------------------------------------------------
+
+struct LimiterParam
+{
+	LimiterParam():
+		limits_tolerance(1.0)
+		{;}
+
+	double limits_tolerance;
+
+	std::vector<double> pos_min_limit_;
+	std::vector<double> pos_max_limit_;
+	std::vector<double> vel_limit_;
+	//std::vector<double> vel_max_limit_;
+};
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+struct ControllerParam
+{
+
+	ControllerParam():
+		dof(0),
+		chain_base_link("arm_base_link"),
+		chain_tip_link("arm_7_link"),
+		root_frame("arm_base_link")
+		{
+			;
+		}
+
+	uint8_t dof;
+	std::string chain_base_link;
+	std::string chain_tip_link;
+	std::string root_frame;
+
+	std::vector<std::string> frame_names;
+	std::vector<std::string> jnts_name;
+
+	LimiterParam lmt_param_;
+
+}; 
+
+//----------------------------------------------------------------------------------------------------------------------------
 
 typedef Eigen::Matrix<double, 6, Eigen::Dynamic> 	Matrix6Xd_t		;
 typedef Eigen::Matrix<double, 6, 7> 				JacobianMatrix	;	//Jacobian matrix for care-o-bot with 7 dof
